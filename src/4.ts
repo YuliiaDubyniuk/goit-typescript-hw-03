c383cf235f1370c8f63f9564a98dc9deae1781f1
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-11-22 22:38:09 -0500

    tasks are done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+    constructor(private signature: number = Math.random()){};
+    getSignature():number {
+        return this.signature;
+    }
+};
+
+class Person{
+    constructor(private key: Key) {}
+    getKey():Key {
+        return this.key;
+    }
+};
+
+abstract class House {
+    tenants: Person[];
+    door: boolean;
+    constructor(public key: Key) {
+    this.door = false;
+  }
+    comeIn(person: Person) {
+        if (this.door) {
+           this.tenants.push(person); 
+        }        
+    }
+
+    abstract openDoor(key: Key):void;
+}
+
+class MyHouse extends House{
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;  
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
